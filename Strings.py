eb17874107b966f40ae68d13a45c8ff01f9d8da8
mallibalogun18 <mallibalogun18@gmail.com>
2020-05-02 14:34:11 -0400

    String python file

---
+++ Strings.py
@@ -0,0 +1,32 @@
+print("Learning Python will help me with AI (Artificial Intelligence).")
+print("Unlike Java, Python doesn't require to define the variable type.")
+print("Hi, " + "My name is Mohammed.")
+print('**********************************************')
+print('Here\'s' + ' an example' + ' of concatenation.')
+print('**********************************************')
+greeting = 'Hello'
+name = 'James Brown'
+print(greeting + ' ' + name)
+print('**********************************************')
+print("There are so much that's needed for a Java program to interact with a user,\nimporting a Scanner class and so "
+      "forth.")
+print('Python doesn\'t require much.')
+print(" ")
+question = "Hello User, your nanme is"
+enterName = input("Please enter your name \n")
+
+print(question + ' ' + enterName)
+
+# here's an example of using tab in python
+print('1\t2\t3\t4')
+
+# using triple quotes allows you to split a string in separate lines
+splitString = """Welcome
+To
+PYTHON!
+Always remember;
+CODE 
+EVERY
+DAY!!!!!!!!!!!!!!"""
+
+print(splitString)
